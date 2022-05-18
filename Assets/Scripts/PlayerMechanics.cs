8773959f4a22c4d424e1e3f0b90e5ec8d8817345
Abdullah Harun Tasdelen <harun6384@gmail.com>
2022-05-18 12:10:56 +0300

    prefabs instatiate

--- Assets/Scripts/PlayerMechanics.cs
+++ Assets/Scripts/PlayerMechanics.cs
@@ -1,31 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class PlayerMechanics : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+
+    [SerializeField] private TMP_Text _score;
+
+    private void Start()
     {
-          
+        _score.text = GameManager.Instance.score.ToString();
     }
-       
-    // Update is called once per frame
-    void Update()
-    {
-         
-    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("BadObj"))
         {
+            GameManager.Instance.score -=20;
+            _score.text = GameManager.Instance.score.ToString();
             other.gameObject.SetActive(false);
-
+            GameManager.Instance.kotuOrman();
         }
         if (other.gameObject.CompareTag("GoodObj"))
         {
+            GameManager.Instance.score +=50;
+            _score.text = GameManager.Instance.score.ToString();
             other.gameObject.SetActive(false);
-
+            GameManager.Instance.iyiOrman();
         }
     }
 }
